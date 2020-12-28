0dbe2006071f5112dc7f484a83a6dd5ef0f359c2
ronanren <renouxpro@gmail.com>
2020-12-28 22:14:02 +0100

    update data

--- home/routes/index.js
+++ home/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const stg = require('../../settings.json')
 var pgp = require('pg-promise')(/* options */)
 var db = pgp('postgres://' + stg['postgresql']['login'] + ':' + stg['postgresql']['password'] + '@' + stg['postgresql']['host'] + ':' + stg['postgresql']['port'] + '/' + stg['postgresql']['database']);
-const fetch = require('node-fetch');
+const axios = require("axios");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -26,30 +26,35 @@ router.get('/product/:productURL', function(req, res, next) {
   var data = [];
   try {
     url = new URL(req.params['productURL']);
-    data.existSite = true;
     data.url = url;
     db.one('SELECT * FROM sites WHERE URL = $1', url.host)
     .then(function (data) {
+      data.existSite = true;
       console.log('DATA:', data);
     })
     .catch(function (error) {
       data.existSite = false;
     })
     data.logo = 'https://www.google.com/s2/favicons?domain=' + url.host;
-    (async () => {
-      const response = await fetch(url + '.json');
-      const json = await response.json();
-      console.log(json);
-    })();
 
+    axios.get(url + ".json")
+         .then((response)=>{
+           data.image = response.data.product.image.src
+         }).catch((error)=>{
+           data.image = "https://via.placeholder.com/400";
+         }).then(function () {
+          console.log(data);
+          res.render('product', { title: 'StopDropshipping', data: data});
+        });;
     
+
+  
   } catch(err) {
     url = "URL invalide, veuillez <a href='/' style='color: purple; text-decoration: none;'>recommencer ici</a>";
     console.log(err)
   }
-  console.log(data);
   // db.one('INSERT INTO sites(URL, NAME, DROPSHIPPING, LOGO) VALUES ($1, $2, $3, $4)', url.host, url.host.split('.')[0], dropshipping, logo)
-  res.render('product', { title: 'StopDropshipping', product: url});
+  // res.render('product', { title: 'StopDropshipping', data: data});
 });
 
 
