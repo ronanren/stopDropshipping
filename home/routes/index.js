9d24c6f722175644ea83d33dfa5b9eab9527e530
ronanren <renouxpro@gmail.com>
2020-12-29 19:30:25 +0100

    ajout page de chaque site

--- home/routes/index.js
+++ home/routes/index.js
@@ -34,22 +34,55 @@ router.get('/product/:productURL', function(req, res, next) {
     data.url = url;
     data.logo = 'https://s2.googleusercontent.com/s2/favicons?domain_url=' + url.host;
     db.one('SELECT * FROM public.sites WHERE URL = $1', url.host)
-    .then(function (data) {})
+    .then(function (site) {
+      data.dropshipping = site.dropshipping;
+    })
     .catch(function (error) {
       db.one('INSERT INTO public.sites(URL, NAME, DROPSHIPPING, LOGO) VALUES ($1, $2, $3, $4)', [url.host, url.host.split('.')[0], false, data.logo]).then(data => {}).catch(error => {});
+      data.dropshipping = false;
     })
     axios.get(url + ".json")
          .then((response)=>{
-           data.image = response.data.product.image.src
-           data.aliseeks = "https://www.aliseeks.com/search/image?aref=undefined&av=1.0.0.4&imageurl=" + data.image;
-           db.one('UPDATE public.sites SET DROPSHIPPING = true WHERE URL = $1', url.host).then(data => {}).catch(error => {});
-           db.one('INSERT INTO public.products(URL_PRODUCT, URL, IMAGE, ALISEEKS) VALUES ($1, $2, $3, $4)', [data.url.href, url.host, data.image, data.aliseeks]).then(data => {}).catch(error => {});
+            data.image = response.data.product.image.src
+            data.dropshipping = true;
+            data.aliseeks = "https://www.aliseeks.com/search/image?aref=undefined&av=1.0.0.4&imageurl=" + data.image;
+            db.one('UPDATE public.sites SET DROPSHIPPING = true WHERE URL = $1', url.host).then(data => {}).catch(error => {});
+            db.one('INSERT INTO public.products(URL_PRODUCT, URL, IMAGE, ALISEEKS) VALUES ($1, $2, $3, $4)', [data.url.href, url.host, data.image, data.aliseeks]).then(data => {}).catch(error => {});
          }).catch((error)=>{
-           data.image = "error";
+            data.image = "error";
          }).then(function () {
-          res.render('product', { title: 'StopDropshipping', data: data});
-        });;
+            console.log(data);
+            res.render('product', { title: 'StopDropshipping', data: data});
+        });
   
+  } catch(err) {
+    url = "URL invalide, veuillez <a href='/' style='color: purple; text-decoration: none;'>recommencer ici</a>";
+    console.log(err)
+  }
+});
+
+/* GET site page. */
+router.get('/site/:siteURL', function(req, res, next) {
+  var data = [];
+  try {
+    url = req.params['siteURL'];
+    data.url = url;
+    data.logo = 'https://s2.googleusercontent.com/s2/favicons?domain_url=' + url;
+    db.any("SELECT * FROM public.products WHERE URL = $1", url)
+    .then(function (products) {
+      if (products.length){
+        data.dropshipping = true;
+      } else {
+        data.dropshipping = false;
+      }
+      data.list_product = products;
+    })
+    .catch(function (error) {
+      data.dropshipping = false;
+    }).then(function () {
+      console.log(data);
+      res.render('site', { title: 'StopDropshipping', data: data});
+    });
   } catch(err) {
     url = "URL invalide, veuillez <a href='/' style='color: purple; text-decoration: none;'>recommencer ici</a>";
     console.log(err)
